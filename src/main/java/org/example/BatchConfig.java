946a297629bcaa7c11608227e0ad2b71f3dab6e2
augus <victor.augusto212@gmail.com>
2024-04-14 23:03:49 -0300

    commit step and config envirements

---
+++ src/main/java/org/example/BatchConfig.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@EnableBatchProcessing
+@Configuration
+public class BatchConfig {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+    @Bean
+    public Job imprimiOlaMundo() {
+        return jobBuilderFactory
+                .get("imprimiOlaMundo")
+                .start(imprimeOlaStep())
+                .incrementer(new RunIdIncrementer())
+                .build();
+    }
+    private Step imprimeOlaStep() {
+        return stepBuilderFactory
+                .get("imprimeOlaSetp")
+                .tasklet(imprimiOlaTasklet(null))
+                .build();
+    }
+    @Bean
+    @StepScope
+    public  Tasklet imprimiOlaTasklet(@Value("#{jobParameters['nome']}") String nome) {
+        return (stepContribution, chunkContext) -> {
+            System.out.println(String.format("Olá, %s!", nome));
+            return RepeatStatus.FINISHED;
+        };
+    }
+}
---
+++ src/main/java/org/example/PrimeiroProjetoBatchApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoBatchApplication.class, args);
+	}
+
+}
